f872bfef0ddee95e458f2295b0db01dc8c9f8620
Robert Tate <btate712@gmail.com>
2019-02-27 15:30:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,4 +17,4 @@ def find_the_cheese# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
-roll_call_dwarves("Doc", "Dopey", "Bashful", "Grumpy")
+roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
