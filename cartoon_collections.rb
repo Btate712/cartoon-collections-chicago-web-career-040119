b4808a6ffd11b1a80afaff41e096c3f8bd65a567
Robert Tate <btate712@gmail.com>
2019-02-27 15:46:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 
 def find_the_cheese(array)
     cheese_types = ["cheddar", "gouda", "camembert"]
-    array.any? do word
-      cheese_types.find(word)
+    array.find do |word|
+      cheese_types.include?(word)
     end
 end
